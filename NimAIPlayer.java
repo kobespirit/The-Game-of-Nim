e8dcf61526b724b4093a2558986e51038a3d4c55
Changjian (Max) Ma <maximilion.ma@gmail.com>
2018-05-11 11:50:46 +1000

    Add files via upload

---
+++ NimAIPlayer.java
@@ -0,0 +1,27 @@
+/*
+	NimAIPlayer.java
+	
+	This class is provided as a skeleton code for the tasks of 
+	Sections 2.3, 2.4 and 2.5 in Project C. Add code (do NOT delete any) to it
+	to finish the tasks. 
+	
+	Coded by: Jin Huang
+	Modified by: Jianzhong Qi, 29/04/2015
+*/
+
+public class NimAIPlayer implements Testable {
+	// you may further extend a class or implement an interface
+	// to accomplish the task in Section 2.3	
+
+	public NimAIPlayer() {
+				
+	}
+	
+	public String advancedMove(boolean[] available, String lastMove) {
+		// the implementation of the victory
+		// guaranteed strategy designed by you
+		String move = "";
+		
+		return move;
+	}
+}
